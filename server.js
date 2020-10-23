1d2b7c0114c2e884dc89b61aeb85df912e0d5c74
JohnLoftin <jgloftin@gmail.com>
2020-10-22 21:08:25 -0400

    complete step 4

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const apiMain = require("./api/main");
+
 const app = express();
 
 app.use("/", express.static(__dirname + "/public"));
 app.use("/css", express.static(__dirname + "/css"));
 app.use("/js", express.static(__dirname + "/js"));
+
+// API routing
+app.use("/", apiMain);
 
 app.get("/heartbeat", (req, res) => {
   res.json({
