9041af75c455a206a1858f0e1f4e4835771486bb
JohnLoftin <jgloftin@gmail.com>
2020-10-22 20:04:47 -0400

    fixed GET issue

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.use("/", express.static(__dirname + "/public"));
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/js", express.static(__dirname + "/js"));
+
 app.get("/heartbeat", (req, res) => {
   res.json({
     is: "working",
