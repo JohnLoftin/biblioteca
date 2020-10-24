9d8db489c7aa9ed9b4a19a3aae6afb7745dedfdd
JohnLoftin <jgloftin@gmail.com>
2020-10-24 12:00:10 -0400

    Completed step five

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+const form = document.getElementById("form");
+
+const stringifyFormData = (fd) => {
+  const data = {};
+  for (let field of fd.keys()) {
+    data[field] = fd.get(field);
+  }
+  return JSON.stringify(data, null, 2);
+};
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const data = new FormData(e.target);
+  const stringified = stringifyFormData(data);
+  console.log(stringified);
+};
+
+form.addEventListener("submit", handleSubmit);
--- server.js
+++ server.js
@@ -1,20 +1,24 @@
+require("dotenv").config();
+
 const express = require("express");
-const apiMain = require("./api/main");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
+
+// const apiMain = require("./api/main");
+const heartbeat = require("./api/apiHeartbeat");
+const apiBooks = require("./api/apiBooks");
 
 const app = express();
 
 app.use("/", express.static(__dirname + "/public"));
 app.use("/css", express.static(__dirname + "/css"));
 app.use("/js", express.static(__dirname + "/js"));
+app.use(bodyParser.json());
 
 // API routing
-app.use("/", apiMain);
-
-app.get("/heartbeat", (req, res) => {
-  res.json({
-    is: "working",
-  });
-});
+// app.use("/", apiMain);
+app.use("/api/apiHeartbeat", heartbeat);
+app.use("/api/apiBooks", apiBooks);
 
 app.listen(3001, () => {
   console.log("Server Running on PORT 3001");
